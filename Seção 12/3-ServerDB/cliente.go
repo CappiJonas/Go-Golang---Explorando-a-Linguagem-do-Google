759636725e9e3630517acd2393003643420011ec
Jonas Cappi <cappijonas@gmail.com>
2022-07-14 09:10:46 -0300

    Desafio do professor implementando os outros endpoints do CRUD e isolando a criação da conexão em um método

--- Seção 12/3-ServerDB/cliente.go
+++ Seção 12/3-ServerDB/cliente.go
@@ -28,6 +28,12 @@ func UsuarioHandler(w http.ResponseWriter, r *http.Request) {
 		usuarioPorID(w, r, id)
 	case r.Method == "GET":
 		usuarioTodos(w, r)
+	case r.Method == "POST":
+		addUsuario(w, r)
+	case r.Method == "DELETE":
+		delUsuario(w, r, id)
+	case r.Method == "PUT":
+		updUsuario(w, r)
 	default:
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprintf(w, "Desculpa... :(")
@@ -35,10 +41,7 @@ func UsuarioHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func usuarioPorID(w http.ResponseWriter, r *http.Request, id int) {
-	db, err := sql.Open("mysql", "root:84123@tcp/cursogo")
-	if err != nil {
-		log.Fatal(err)
-	}
+	db := createConnection()
 	defer db.Close()
 
 	var u Usuario
@@ -51,10 +54,7 @@ func usuarioPorID(w http.ResponseWriter, r *http.Request, id int) {
 }
 
 func usuarioTodos(w http.ResponseWriter, r *http.Request) {
-	db, err := sql.Open("mysql", "root:84123@tcp/cursogo")
-	if err != nil {
-		log.Fatal(err)
-	}
+	db := createConnection()
 	defer db.Close()
 
 	rows, _ := db.Query("select id, nome from usuarios")
@@ -71,4 +71,55 @@ func usuarioTodos(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprintf(w, string(json))
+}
+
+func addUsuario(w http.ResponseWriter, r *http.Request) {
+	db := createConnection()
+	defer db.Close()
+
+	var u Usuario
+	json.NewDecoder(r.Body).Decode(&u)
+
+	db.Query("insert into usuarios(nome) values(?)", u.Nome)
+
+	json, _ := json.Marshal("Usuário inserido com sucesso")
+
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, string(json))
+}
+
+func delUsuario(w http.ResponseWriter, r *http.Request, id int) {
+	db := createConnection()
+	defer db.Close()
+
+	db.Query("delete from usuarios where id = ?", id)
+
+	json, _ := json.Marshal("Usuário deletado com sucesso")
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, string(json))
+}
+
+func updUsuario(w http.ResponseWriter, r *http.Request) {
+	db := createConnection()
+	defer db.Close()
+
+	var u Usuario
+	json.NewDecoder(r.Body).Decode(&u)
+
+	db.Query("update usuarios set nome = ? where id = ?", u.Nome, u.ID)
+
+	json, _ := json.Marshal("Usuário atualizado com sucesso")
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, string(json))
+}
+
+func createConnection() *sql.DB {
+	db, err := sql.Open("mysql", "root:84123@tcp/cursogo")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
 }
